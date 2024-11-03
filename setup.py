682b01937e931e6866a6093b8da0c483517abdf2
Joachim Meyer <jmeyer@boisestate.edu>
2024-11-03 10:33:12 -0700

    Setup.py - Refactor building extensions
    Rework compiling the Cython extensions or falling back to using
    setuptools with the help of this documentation:
    https://cython.readthedocs.io/en/stable/src/userguide/source_files_and_compilation.html
    
    Not (yet) adding Cython as build requirement to enable building on
    systems that don't have Cython installed.

--- setup.py
+++ setup.py
@@ -1,36 +1,27 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import os
 
 import numpy
 from setuptools import Extension, setup
 
-# Test if compiling with cython or using the C source
 try:
-    from Cython.Distutils import build_ext as _build_ext
-except ImportError:
-    from setuptools.command.build_ext import build_ext as _build_ext
-    ext = '.c'
-    print('Using GCC')
-else:
-    ext = '.pyx'
+    # Use the Cython build_ext module if the library is installed.
+    from Cython.Distutils import build_ext
     print('Using Cython')
-
-
-def c_name_from_path(location, name):
-    return os.path.join(location, name).replace('/', '.')
-
-
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-
+    ext = '.pyx'
+except ImportError:
+    # Falling back to setuptools as the default if the Cython import is
+    # unsuccessful
+    from setuptools.command.build_ext import build_ext
+    print('Using GCC')
+    ext = '.c'
 
 # Give user option to specify local compiler name
 if "CC" not in os.environ:
     os.environ["CC"] = "gcc"
-print(os.environ["CC"])
+
+print("Compiler set to: " + os.environ["CC"])
 
 # extension parameters
 extension_params = dict(
@@ -39,48 +30,51 @@ extension_params = dict(
     include_dirs=[numpy.get_include()],
 )
 
-ext_modules = []
+extensions = []
 
 # detrended kriging
-source_folder = 'smrf/spatial/dk'
-ext_modules += [
+extensions += [
     Extension(
-        c_name_from_path(source_folder, 'detrended_kriging'),
-        sources=[os.path.join(source_folder, val) for val in [
-            "detrended_kriging" + ext,
-            "krige.c",
-            "lusolv.c",
-            "array.c"
-        ]],
+        'smrf.spatial.dk.detrended_kriging',
+        sources=[
+            os.path.join('smrf/spatial/dk', source_file) for source_file in [
+                "detrended_kriging" + ext,
+                "krige.c",
+                "lusolv.c",
+                "array.c"
+            ]
+        ],
         **extension_params
     ),
 ]
 
 # envphys core c functions
-source_folder = 'smrf/envphys/core'
-ext_modules += [
+extensions += [
     Extension(
-        c_name_from_path(source_folder, 'envphys_c'),
-        sources=[os.path.join(source_folder, val) for val in [
-            "envphys_c" + ext,
-            "topotherm.c",
-            "dewpt.c",
-            "iwbt.c"
-        ]],
+        'smrf.envphys.core.envphys_c',
+        sources=[
+            os.path.join('smrf/envphys/core', val) for val in [
+                "envphys_c" + ext,
+                "topotherm.c",
+                "dewpt.c",
+                "iwbt.c"
+            ]
+        ],
         **extension_params
     ),
 ]
 
 # wind model c functions
-source_folder = 'smrf/utils/wind'
-ext_modules += [
+extensions += [
     Extension(
-        c_name_from_path(source_folder, 'wind_c'),
-        sources=[os.path.join(source_folder, val) for val in [
-            "wind_c" + ext,
-            "breshen.c",
-            "calc_wind.c"
-        ]],
+        'smrf.utils.wind.wind_c',
+        sources=[
+            os.path.join('smrf/utils/wind', val) for val in [
+                "wind_c" + ext,
+                "breshen.c",
+                "calc_wind.c"
+            ]
+        ],
         **extension_params
     ),
 ]
@@ -89,5 +83,5 @@ setup(
     cmdclass={
         'build_ext': build_ext
     },
-    ext_modules=ext_modules,
+    ext_modules=extensions,
 )
