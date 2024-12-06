127b7cd4b5476ce8682144f192559c7c038dfa19
Joachim Meyer <jmeyer@boisestate.edu>
2024-12-06 14:22:56 -0700

    HRRR data - Introduce dataclass to map HRRR to SMRF variables
    Introduce a dataclass that holds information to load HRRR variables from
    WGRIB files and maps to the corresponding SMRF variable.

---
+++ smrf/data/hrrr/grib_file_variables.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class HrrrVariable:
+    """
+    Class holding information to load HRRR variables from
+    WGRIB files and maps to the corresponding SMRF variable.
+    """
+    level: str
+    grib_identifier: str
+    grib_keys: dict
+    smrf_map: dict
+
+
+# HRRR variables
+FIRST_HOUR = {'stepRange': ['1', '0-1']}
+SIXTH_HOUR = {'stepRange': ['6', '5-6']}
+SURFACE = 'surface'
+HAG = 'heightAboveGround'
+
+HRRR_SURFACE = HrrrVariable(
+    level=SURFACE,
+    grib_identifier='shortName',
+    grib_keys={
+        'level': 0,
+        'typeOfLevel': SURFACE,
+    },
+    smrf_map={
+        'orog': 'elevation',
+        'sdswrf': 'short_wave',
+        'tp': 'precip_int',
+    }
+)
+HRRR_HAG_2 = HrrrVariable(
+    level=HAG,
+    grib_identifier='cfVarName',
+    grib_keys={
+        'level': 2,
+        'typeOfLevel': HAG,
+    },
+    smrf_map={
+        'r2': 'relative_humidity',
+        't2m': 'air_temp',
+    },
+)
+HRRR_HAG_10 = HrrrVariable(
+    level=HAG,
+    grib_identifier='cfVarName',
+    grib_keys={
+        'level': 10,
+        'typeOfLevel': HAG,
+    },
+    smrf_map={
+        'u10': 'wind_u',
+        'v10': 'wind_v',
+
+    },
+)
