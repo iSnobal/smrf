cb6408b30bb307bb545ba19b989325fb270e9a18
Joachim Meyer <jmeyer@boisestate.edu>
2025-10-08 17:26:31 -0600

    Distribute - Albedo - Lint code

--- smrf/distribute/albedo.py
+++ smrf/distribute/albedo.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from typing import Tuple
+
 import numpy as np
 
 from .image_data import ImageData
@@ -44,13 +46,12 @@ class Albedo(ImageData):
 
         # Get the veg values for the decay methods. Date method uses self.veg
         # Hardy2000 uses self.litter
-        for d in ['veg', 'litter']:
+        for d in ["veg", "litter"]:
             v = {}
 
-            matching = [s for s in config.keys()
-                        if "{0}_".format(d) in s]
+            matching = [s for s in config.keys() if "{0}_".format(d) in s]
             for m in matching:
-                ms = m.split('_')
+                ms = m.split("_")
                 v[ms[-1]] = config[m]
 
             # Create self.litter,self.veg
@@ -72,52 +73,32 @@ class Albedo(ImageData):
         if self.config["decay_method"] is None:
             self._logger.warning("No decay method is set!")
 
-    def distribute(self, current_time_step, cosz, storm_day):
+    def distribute(
+        self, current_time_step: datetime, cosz: np.ndarray, storm_day: np.ndarray
+    ) -> None:
         """
         Distribute air temperature given a Panda's dataframe for a single time
         step. Calls :mod:`smrf.distribute.ImageData._distribute`.
 
         Args:
             current_time_step: Current time step in datetime object
-            cosz: numpy array of the illumination angle for the current time
-                step
-            storm_day: numpy array of the decimal days since it last
-                snowed at a grid cell
-
+            cosz: Llumination angle for the current time step
+            storm_day: Decimal days since it last snowed at a grid cell
         """
 
-        self._logger.debug('%s Distributing albedo' % current_time_step)
+        self._logger.debug("%s Distributing albedo" % current_time_step)
 
         # only need to calculate albedo if the sun is up
         if cosz is not None:
-
             alb_v, alb_ir = albedo.albedo(
-                storm_day, cosz,
-                self.config['grain_size'],
-                self.config['max_grain'],
-                self.config['dirt'])
+                storm_day,
+                cosz,
+                self.config["grain_size"],
+                self.config["max_grain"],
+                self.config["dirt"],
+            )
 
             # Perform litter decay
-            if self.config['decay_method'] == 'date_method':
-                alb_v_d, alb_ir_d = albedo.decay_alb_power(
-                    self.veg,
-                    self.veg_type,
-                    self.config['date_method_start_decay'],
-                    self.config['date_method_end_decay'],
-                    current_time_step,
-                    self.config['date_method_decay_power'],
-                    alb_v, alb_ir)
-
-                alb_v = alb_v_d
-                alb_ir = alb_ir_d
-
-            elif self.config['decay_method'] == 'hardy2000':
-                alb_v_d, alb_ir_d = albedo.decay_alb_hardy(
-                    self.litter,
-                    self.veg_type,
-                    storm_day,
-                    alb_v,
-                    alb_ir)
             if self.config["decay_method"] == "date_method":
                 current_hours, decay_hours = self.decay_window(current_time_step)
                 if current_hours > 0:
@@ -131,8 +112,10 @@ class Albedo(ImageData):
                         alb_ir,
                     )
 
-                alb_v = alb_v_d
-                alb_ir = alb_ir_d
+            elif self.config["decay_method"] == "hardy2000":
+                alb_v, alb_ir = albedo.decay_alb_hardy(
+                    self.litter, self.veg_type, storm_day, alb_v, alb_ir
+                )
 
             self.albedo_vis = utils.set_min_max(alb_v, self.min, self.max)
             self.albedo_ir = utils.set_min_max(alb_ir, self.min, self.max)
