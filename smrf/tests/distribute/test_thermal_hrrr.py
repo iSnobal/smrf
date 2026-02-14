f83a4004b0ddd40354fcd19d6405c83ed6d01898
Joachim Meyer <jmeyer@boisestate.edu>
2026-02-14 16:06:07 -0700

    Distribute - Thermal HRRR - Add ability to correct for vegetation
    Mirror the implementation of the original thermal calculation and allow to correct the
    HRRR thermal radiation by vegetation via configuration parameter `correct_veg` in the .ini
    file.

--- smrf/tests/distribute/test_thermal_hrrr.py
+++ smrf/tests/distribute/test_thermal_hrrr.py
@@ -1,29 +1,52 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import numpy as np
-from smrf.data import Topo
-from smrf.distribute import ThermalHRRR
-from smrf.envphys.constants import EMISS_TERRAIN, STEF_BOLTZ, FREEZE
+import numpy.testing as npt
+import pandas as pd
 
-SKY_VIEW_FACTOR_MOCK = np.ones((1, 1))
+from smrf.distribute import ThermalHRRR
+from smrf.envphys.constants import EMISS_TERRAIN, FREEZE, STEF_BOLTZ
+from smrf.tests.distribute import SKY_VIEW_FACTOR_MOCK, TOPO_MOCK
+
 RAW_DATA_MOCK = np.ones((1, 1))
 AIR_TEMP_MOCK = np.ones((1, 1))
 
-TOPO_MOCK = MagicMock(spec=Topo, sky_view_factor=SKY_VIEW_FACTOR_MOCK, instance=True)
 DATA_MOCK = MagicMock(thermal=RAW_DATA_MOCK)
+
+CONFIG = {
+    "time": {
+        "start_date": "2025-09-20 00:00",
+        "time_zone": "utc",
+    },
+    "thermal": {
+        "correct_veg": False,
+    },
+}
 
 
 class TestThermalHRRR(unittest.TestCase):
-    def setUp(self):
-        self.subject = ThermalHRRR(topo=TOPO_MOCK)
-
     def test_distribute(self):
+        self.subject = ThermalHRRR(config=CONFIG, topo=TOPO_MOCK)
+
         result = (SKY_VIEW_FACTOR_MOCK * RAW_DATA_MOCK) + (
             1 - SKY_VIEW_FACTOR_MOCK
-        ) * EMISS_TERRAIN * STEF_BOLTZ * (AIR_TEMP_MOCK + FREEZE)**4
+        ) * EMISS_TERRAIN * STEF_BOLTZ * (AIR_TEMP_MOCK + FREEZE) ** 4
 
-        self.subject.initialize(None)
+        self.subject.initialize(pd.DataFrame())
         self.subject.distribute("2025-09-20=9", RAW_DATA_MOCK, AIR_TEMP_MOCK)
 
-        self.assertEqual(result, self.subject.thermal)
+        npt.assert_equal(result, self.subject.thermal)
+
+    @patch("smrf.distribute.thermal_hrrr.vegetation")
+    def test_distribute_vegetation(self, mock_vegetation):
+        CONFIG["thermal"]["correct_veg"] = True
+        self.subject = ThermalHRRR(config=CONFIG, topo=TOPO_MOCK)
+
+        self.subject.initialize(pd.DataFrame())
+        self.subject.distribute("2025-09-20=9", RAW_DATA_MOCK, AIR_TEMP_MOCK)
+
+        mock_vegetation.thermal_correct_canopy.assert_called_once()
+
+        # Reset to original
+        CONFIG["thermal"]["correct_veg"] = False
