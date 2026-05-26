366118861f555ec3b2b4c3632799aa38b8f9a3c4
Joachim Meyer <jmeyer@boisestate.edu>
2026-05-26 15:09:28 -0600

    Tests - Wind - Drop comparison to gold files in unit tests
    Those are compared when running the overarching TestLakes suite.

--- smrf/tests/distribute/wind/test_wind_ninja.py
+++ smrf/tests/distribute/wind/test_wind_ninja.py
@@ -1,4 +1,3 @@
-import netCDF4 as nc
 import numpy as np
 import pandas as pd
 import pytz
@@ -6,7 +5,6 @@ import pytz
 from smrf.data import Topo
 from smrf.distribute.wind.wind import Wind
 from smrf.tests.smrf_test_case_lakes import SMRFTestCaseLakes
-from smrf.utils import utils
 from smrf.utils.utils import date_range
 
 
@@ -17,17 +15,15 @@ class TestWindNinja(SMRFTestCaseLakes):
         Args:
             config (dict): config dict
         """
-
-        # Load the topo
-        topo = Topo(config['topo'])
+        topo = Topo(config["topo"])
 
         # Get the time steps correctly in the time zone
-        tzinfo = pytz.timezone(config['time']['time_zone'])
+        tzinfo = pytz.timezone(config["time"]["time_zone"])
         date_time = date_range(
-            config['time']['start_date'],
-            config['time']['end_date'],
-            config['time']['time_step'],
-            tzinfo
+            config["time"]["start_date"],
+            config["time"]["end_date"],
+            config["time"]["time_step"],
+            tzinfo,
         )
 
         wind = Wind(config=config, topo=topo)
@@ -49,31 +45,10 @@ class TestWindNinja(SMRFTestCaseLakes):
         # The y values are descending
         self.assertTrue(np.all(np.diff(wn.windninja_y) < 0))
 
-        # compare against gold
-        g_vel = utils.set_min_max(
-            g_vel, wn.config['min'], wn.config['max'])
-
-        # check against gold
-        # The two are not exactly the same as there is some float
-        # precision error with netcdf
-        n = nc.Dataset(self.gold_dir.joinpath('wind_speed.nc'))
-        np.testing.assert_allclose(
-            n.variables['wind_speed'][0, :],
-            g_vel
-        )
-        n.close()
-
-        n = nc.Dataset(self.gold_dir.joinpath('wind_direction.nc'))
-        np.testing.assert_allclose(
-            n.variables['wind_direction'][0, :],
-            g_ang
-        )
-        n.close()
-
     def test_wind_ninja_interpolation(self):
 
         config = self.base_config_copy().cfg
-        config['wind']['wind_ninja_dxdy'] = 50
+        config["wind"]["wind_ninja_dxdy"] = 50
         topo, wn, g_vel, g_ang = self.setup_wind_ninja(config)
 
         # The implied assumption is that this does not throw an
