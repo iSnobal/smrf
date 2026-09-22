5e77b8fc0beb8f0a4bc31af4f3e8a754c1492883
J. Michelle Hu <43484205+jmichellehu@users.noreply.github.com>
2026-09-22 13:47:02 -0600

    Tests - hrrr, lakes - correct path bug input to compare_netcdf_files
    Call sites input absolute path and overrides output_dir path.
    Skips gold comparison of hrrr_solar.nc/solar_k.nc.
    Replace with filename to compare correct source and output files.

--- smrf/tests/data/hrrr/test_hrrr.py
+++ smrf/tests/data/hrrr/test_hrrr.py
@@ -86,8 +86,8 @@ class TestHrrrSolar(SMRFTestCaseLakes):
             with nc.Dataset(self.output_dir.joinpath("net_solar.nc")) as test:
                 self.compare_file_variables(gold, test)
 
-        self.compare_netcdf_files(self.gold_dir.joinpath("hrrr_solar.nc"))
-        self.compare_netcdf_files(self.gold_dir.joinpath("solar_k.nc"))
+        self.compare_netcdf_files("hrrr_solar.nc")
+        self.compare_netcdf_files("solar_k.nc")
 
     def test_load_with_vegetation(self):
         config = self.base_config_copy()
